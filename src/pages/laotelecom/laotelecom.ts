e6bdd103e8f948729c04d363435c39c16801cb75
mouathor999 <mouathorgithub@gmail.com>
2017-10-05 14:22:15 +0700

    add laotelecom net sim

--- src/pages/laotelecom/laotelecom.ts
+++ src/pages/laotelecom/laotelecom.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import {CallNumber} from '@ionic-native/call-number';
 
 /**
  * Generated class for the LaotelecomPage page.
@@ -15,13 +16,49 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LaotelecomPage {
 title="Hello";
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public alertCtr:AlertController,
+              public call: CallNumber
+  ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LaotelecomPage');
   }
-  SayHello(){
-    console.log("Hello laotelecom");
+
+  //Refresher function
+
+  refreshPage(refresh){
+    setTimeout(() => {
+      refresh.complete();
+    }, 3000);
+  }
+
+
+
+  RegisterSimPackage(packageCode:string){
+    const alert = this.alertCtr.create({
+      title:"Package",
+      message:"ສະໝັກເພັກເກັກນີ້ບໍ ?",
+      buttons: [
+        {
+          text:"ຍົກເລີກ",
+          handler:()=>{
+            // cancel coding here
+          }
+        },
+        {
+          text:"ສະໝັກ",
+          handler:()=>{
+            const buypackage = "*131*" + packageCode + "#";
+            this.call.callNumber(buypackage,true).then(()=>{
+               alert.dismiss();
+            });
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 }
