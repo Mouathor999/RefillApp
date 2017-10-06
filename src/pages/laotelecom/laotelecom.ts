64fd5cb3d6d54bbdc995909d830874a7718ffba6
mouathor999 <mouathorgithub@gmail.com>
2017-10-06 23:47:25 +0700

    finished laotelecom

--- src/pages/laotelecom/laotelecom.ts
+++ src/pages/laotelecom/laotelecom.ts
@@ -36,9 +36,9 @@ title="Hello";
   }
 
 
-
-  RegisterSimPackage(packageCode:string){
-    const alert = this.alertCtr.create({
+  // Sim net package
+  RegisterNetSimPackage(packageNetCode:string){
+    const alert1 = this.alertCtr.create({
       title:"Package",
       message:"ສະໝັກເພັກເກັກນີ້ບໍ ?",
       buttons: [
@@ -51,14 +51,134 @@ title="Hello";
         {
           text:"ສະໝັກ",
           handler:()=>{
-            const buypackage = "*131*" + packageCode + "#";
+            const buypackage = "*131*" + packageNetCode + "#";
             this.call.callNumber(buypackage,true).then(()=>{
-               alert.dismiss();
+               alert1.dismiss();
             });
           }
         }
       ]
     });
-    alert.present();
+    alert1.present();
   }
+
+
+  //Wifi package
+  RegisterWifiPackage(packageWifiCode){
+    const alert2 = this.alertCtr.create({
+      title:"Package",
+      message:"ສະໝັກເພັກເກັກນີ້ບໍ ?",
+      buttons: [
+        {
+          text:"ຍົກເລີກ",
+          handler:()=>{
+            // cancel coding here
+          }
+        },
+        {
+          text:"ສະໝັກ",
+          handler:()=>{
+            const buypackage = "*155*3*" + packageWifiCode + "#";
+            this.call.callNumber(buypackage,true).then(()=>{
+              alert2.dismiss();
+            });
+          }
+        }
+      ]
+    });
+    alert2.present();
+  }
+
+  // Call package
+  RegisterCallSimPackage(packageCallCode:string){
+    const alert3 = this.alertCtr.create({
+      title:"Package",
+      message:"ສະໝັກເພັກເກັກນີ້ບໍ ?",
+      buttons: [
+        {
+          text:"ຍົກເລີກ",
+          handler:()=>{
+            // cancel coding here
+          }
+        },
+        {
+          text:"ສະໝັກ",
+          handler:()=>{
+            const buypackage = "*188*" + packageCallCode + "#";
+            this.call.callNumber(buypackage,true).then(()=>{
+              alert3.dismiss();
+            });
+          }
+        }
+      ]
+    });
+    alert3.present();
+  }
+
+  // Monthl call package
+
+  RegisterMonthlySimPackage(packageMonthlycode:string){
+
+    const alert4 = this.alertCtr.create({
+      title: "Package",
+      message: "ສະໝັກເພັກເກັກນີ້ບໍ ?",
+      buttons: [
+        {
+         text:"ຍົກເລີກ",
+          handler:()=>{
+          // Cancel code here
+          }
+        },
+        {
+         text:"ສະໝັກ",
+          handler:()=>{
+            const Monthlypackage = "*111*"+ packageMonthlycode +"#";
+            this.call.callNumber(Monthlypackage,true).then(()=>{
+              alert4.dismiss();
+            });
+          }
+        }
+      ]
+    });
+    alert4.present();
+  }
+
+//  If uaser click button on LTC service page it will process this function as call to ltc call center service
+  CallCenter(){
+    const Callalert = this.alertCtr.create({
+      title: "ການໂທ",
+      message: "ທ່ານຕ້ອງການໂທຫາສູນບໍລິການຂອງລາວໂທລະຄົມບໍ ?",
+      buttons: [
+        {
+          text:"ຍົກເລີກ",
+          handler:()=>{
+            // Cancel code here
+          }
+        },
+        {
+          text:"ໂທອອກ",
+          handler:()=>{
+            const callcenterCode="121";
+            this.call.callNumber(callcenterCode,true).then(()=>{
+              Callalert.dismiss();
+            });
+          }
+        }
+      ]
+    });
+    Callalert.present();
+  }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
